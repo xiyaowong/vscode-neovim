35b318349de0faf06d59873508fe33409ad06dae
wongxy <xiyao.wong@foxmail.com>
2024-06-21 21:36:02 +0800

    test: test

--- src/test/integ/index.ts
+++ src/test/integ/index.ts
@@ -1,8 +1,8 @@
 import path from "path";
 
-import * as vscode from "vscode";
 import Mocha from "mocha";
 import "source-map-support/register";
+import * as vscode from "vscode";
 
 export async function run(): Promise<void> {
     let test_regex = process.env.NEOVIM_TEST_REGEX;
@@ -20,34 +20,37 @@ export async function run(): Promise<void> {
         fullTrace: true,
         grep: test_regex,
         retries: 2,
+        require: ["mocha-suprress-logs"],
     });
     const testsRoot = path.resolve(__dirname, "..");
 
     return new Promise((c, e) => {
-        return vscode.workspace.findFiles(new vscode.RelativePattern(vscode.Uri.file(testsRoot), "**/**.test.js")).then(
-            (testFiles) => {
-                // Add files to the test suite, in alphanumeric order.
-                testFiles.sort((a, b) => a.fsPath.localeCompare(b.fsPath));
-                for (const f of testFiles) {
-                    mocha.addFile(path.resolve(testsRoot, f.fsPath));
-                }
+        return vscode.workspace
+            .findFiles(new vscode.RelativePattern(vscode.Uri.file(testsRoot), "**/buffer-write-cmd.test.js"))
+            .then(
+                (testFiles) => {
+                    // Add files to the test suite, in alphanumeric order.
+                    testFiles.sort((a, b) => a.fsPath.localeCompare(b.fsPath));
+                    for (const f of testFiles) {
+                        mocha.addFile(path.resolve(testsRoot, f.fsPath));
+                    }
 
-                try {
-                    // Run the mocha test
-                    mocha.run((failures) => {
-                        if (failures > 0) {
-                            e(new Error(`${failures} tests failed.`));
-                        } else {
-                            c();
-                        }
-                    });
-                } catch (err) {
+                    try {
+                        // Run the mocha test
+                        mocha.run((failures) => {
+                            if (failures > 0) {
+                                e(new Error(`${failures} tests failed.`));
+                            } else {
+                                c();
+                            }
+                        });
+                    } catch (err) {
+                        e(err);
+                    }
+                },
+                (err) => {
                     e(err);
-                }
-            },
-            (err) => {
-                e(err);
-            },
-        );
+                },
+            );
     });
 }
